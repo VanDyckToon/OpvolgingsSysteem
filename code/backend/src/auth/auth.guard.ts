1b9679157ac6d11201bc1bbbc6ee2c90c176b30c
Seppe Stroobants <s.stroobants@hotmail.com>
2024-10-18 13:49:25 +0200

    auth

--- code/backend/src/auth/auth.guard.ts
+++ code/backend/src/auth/auth.guard.ts
@@ -6,12 +6,26 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const IsPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (IsPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
@@ -21,14 +35,15 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
-      request['gebruiker'] = payload;
+
+      request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
     }
     return true;
   }
 
-  private extractTokenFromHeader(request: any): string | undefined {
+  private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
--- code/backend/src/auth/auth.service.ts
+++ code/backend/src/auth/auth.service.ts
@@ -6,25 +6,20 @@ import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
-    private gebruikerService: GebruikerService,
-    private jwtService: JwtService,
+    private readonly gebruikerService: GebruikerService,
+    private readonly jwtService: JwtService,
   ) {}
 
-  async signIn(
-    email: string,
-    password: string,
-  ): Promise<{ access_token: string }> {
+  async signIn(email: string, pass: string): Promise<{ access_token: string }> {
     const gebruiker = await this.gebruikerService.findGebruikerByEmail(email);
-    if (!gebruiker || !(await bcrypt.compare(password, gebruiker.wachtwoord))) {
+    if (!gebruiker || !(await bcrypt.compare(pass, gebruiker.wachtwoord))) {
       throw new UnauthorizedException();
     }
     const payload = {
       gebruikerID: gebruiker.gebruikerID,
+      rol: gebruiker.rol,
       email: gebruiker.email,
-      rol: gebruiker.rol.naam,
     };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
