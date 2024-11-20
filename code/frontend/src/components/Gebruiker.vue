adf98e7519239f6b0c283c3fe36f6c817979fb09
Seppe Stroobants <s.stroobants@hotmail.com>
2024-11-20 13:20:45 +0100

    it works for now

--- code/frontend/src/components/Gebruiker.vue
+++ code/frontend/src/components/Gebruiker.vue
@@ -635,102 +635,6 @@
             Verwijder
           </button>
         </div>
-
-    <HeaderComponent :begeleiderID="begeleiderID" />
-    <div class="flex-grow flex justify-center items-center">
-      <div class="w-full max-w-4xl p-8 bg-white shadow-lg rounded-lg">
-        <h2 class="text-2xl font-bold mb-6 text-center text-[#456A50]">
-          Werknemer(s)
-        </h2>
-
-        <!-- Search Input -->
-        <div class="mb-4">
-          <input
-            type="text"
-            v-model="searchQuery"
-            placeholder="Zoek gebruikers..."
-            class="w-full p-2 border border-gray-300 rounded"
-          />
-        </div>
-
-        <ul v-if="filteredGebruikers.length" class="divide-y divide-gray-200">
-          <li
-            v-for="gebruiker in filteredGebruikers"
-            :key="gebruiker.gebruikerID"
-            class="py-4 flex items-center justify-between"
-          >
-            <div class="flex items-center">
-              <!-- Profile Image -->
-              <img
-                v-if="gebruiker.foto"
-                :src="`/assets/${gebruiker.foto}.jpg`"
-                alt="Profile picture"
-                class="w-10 h-10 object-cover rounded-full mr-4"
-              />
-              <img
-                v-else
-                :src="`/assets/no_image_available.jpg`"
-                alt="No picture available"
-                class="w-10 h-10 object-cover rounded-full mr-4"
-              />
-              <!-- User Details -->
-              <div>
-                <div class="text-[#456A50] font-bold">
-                  {{ gebruiker.voornaam }} {{ gebruiker.achternaam }}
-                </div>
-                <div class="text-gray-400 text-sm">
-                  {{ gebruiker.telefoonnummer }}
-                </div>
-              </div>
-            </div>
-
-            <!-- Icons Section -->
-            <div class="flex space-x-4">
-              <button>
-                <img
-                  src="../assets/competentiester.svg"
-                  alt="competentiester"
-                  class="h-8 w-auto object-contain color-[#101234]"
-                  @click="goToCompetenties(gebruiker.gebruikerID)"
-                />
-              </button>
-              <button>
-                <img
-                  src="../assets/technischecompententie.svg"
-                  alt="technischecompententie"
-                  class="h-8 w-auto object-contain color-[#101234]"
-                  @click="goToTechnischeCompetenties(gebruiker.gebruikerID)"
-                />
-              </button>
-              <button>
-                <img
-                  src="../assets/more.svg"
-                  alt="more"
-                  class="h-8 w-auto object-contain color-[#101234]"
-                  @click="goToDetails(gebruiker.gebruikerID)"
-                />
-              </button>
-              <button>
-                <img
-                  src="../assets/opleiding-icoon.svg"
-                  alt="comment"
-                  class="h-8 w-auto object-contain color-[#101234]"
-                  @click="goToOpleidingen(gebruiker.gebruikerID)"
-                />
-              </button>
-              <button>
-                <img
-                  src="../assets/comment.svg"
-                  alt="comment"
-                  class="h-8 w-auto object-contain color-[#101234]"
-                  @click="goToOpmerkingen(gebruiker.gebruikerID)"
-                />
-              </button>
-            </div>
-          </li>
-        </ul>
-        <p v-else class="text-center text-gray-500">Geen gebruikers gevonden</p>
-
       </div>
     </div>
   </div>
@@ -739,7 +643,6 @@
 <script lang="ts">
 import axios from 'axios'
 import { defineComponent } from 'vue'
-
 import { Icon } from '@iconify/vue'
 import HeaderComponent from '../components/Header.vue'
 
@@ -773,11 +676,6 @@ interface Rol {
   naam: string
 }
 
-
-import HeaderComponent from '../components/Header.vue'
-import { jwtDecode } from 'jwt-decode'
-
-
 interface Gebruiker {
   gebruikerID: number
   voornaam: string
@@ -801,6 +699,7 @@ interface Gebruiker {
 export default defineComponent({
   name: 'Gebruiker',
   components: {
+    Icon,
     HeaderComponent,
   },
   data() {
@@ -1037,35 +936,10 @@ export default defineComponent({
         this.isDeleteModalVisible = false // Close the modal after confirmation
       } catch (error) {
         console.error('Error deleting gebruiker:', error)
-      gebruikers: [] as Gebruiker[],
-      begeleiderID: this.$route.params.id as string,
-      rolID: null as string | null,
-      searchQuery: '', // Add a search query property
-    }
-  },
-  async mounted() {
-    const token = localStorage.getItem('access_token')
-    if (token) {
-      try {
-        const decodedToken = jwtDecode(token) as { rol: { rolID: string } }
-        this.rolID = decodedToken.rol.rolID
-      } catch (error) {
-        console.error('Error decoding token:', error)
       }
-    }
-    this.fetchGebruikers()
-  },
-  computed: {
-    filteredGebruikers() {
-      return this.gebruikers.filter(gebruiker => {
-        const fullName =
-          `${gebruiker.voornaam} ${gebruiker.achternaam}`.toLowerCase()
-        return fullName.includes(this.searchQuery.toLowerCase()) // Filter by name
-      })
     },
-  },
-  methods: {
-    async fetchGebruikers() {
+
+    async deleteGebruiker(gebruikerID: number) {
       try {
         const token = localStorage.getItem('access_token')
         await axios.delete(`http://localhost:3000/gebruiker/${gebruikerID}`, {
@@ -1075,35 +949,11 @@ export default defineComponent({
       } catch (error) {
         console.error(
           'Er is een fout opgetreden bij het verwijderen van de gebruiker:',
-        if (this.rolID == '1') {
-          // Fetch users with rolID 3
-          const response = await axios.get(
-            'http://localhost:3000/gebruiker/rol/3',
-            {
-              headers: { Authorization: `Bearer ${token}` },
-            },
-          )
-          this.gebruikers = response.data
-        } else if (this.rolID == '2') {
-          const response = await axios.get(
-            `http://localhost:3000/gebruiker/begeleider/${this.begeleiderID}`,
-            { headers: { Authorization: `Bearer ${token}` } },
-          )
-          this.gebruikers = response.data
-        } else {
-          const response = await axios.get('http://localhost:3000/admin', {
-            headers: { Authorization: `Bearer ${token}` },
-          })
-          this.gebruikers = response.data
-        }
-      } catch (error) {
-        console.error(
-          'Er is een fout opgetreden bij het ophalen van de gebruikers:',
           error,
         )
       }
     },
-    
+
     openEditModal(
       gebruikerID: number,
       voornaam: string,
@@ -1204,40 +1054,3 @@ export default defineComponent({
   },
 })
 </script>
-    goToDetails(gebruikerID: number) {
-      this.$router.push({
-        name: 'GebruikerDetail',
-        params: { id: gebruikerID, begeleiderID: this.begeleiderID },
-      })
-    },
-    goToOpmerkingen(gebruikerID: number) {
-      this.$router.push({
-        name: 'OpmerkingenPage',
-        params: { id: gebruikerID, begeleiderID: this.begeleiderID },
-      })
-    },
-    goToCompetenties(gebruikerID: number) {
-      this.$router.push({
-        name: 'CompetentiesPage',
-        params: { id: gebruikerID, begeleiderID: this.begeleiderID },
-      })
-    },
-    goToTechnischeCompetenties(gebruikerID: number) {
-      this.$router.push({
-        name: 'TechnischeCompetentiesPage',
-        params: { id: gebruikerID, begeleiderID: this.begeleiderID },
-      })
-    },
-    goToOpleidingen(gebruikerID: number) {
-      this.$router.push({
-        name: 'OpleidingGebruikerPage',
-        params: { id: gebruikerID, begeleiderID: this.begeleiderID },
-      })
-    },
-  },
-})
-</script>
-
-<style scoped>
-/* Voeg hier eventueel extra stijlen toe als dat nodig is */
-</style>
