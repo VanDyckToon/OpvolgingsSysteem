030065a2d478a8c69c545ac63075ef834f59cf64
Seppe Stroobants <s.stroobants@hotmail.com>
2024-12-17 15:13:09 +0100

    small changes

--- code/backend/src/typeorm/dataSource.ts
+++ code/backend/src/typeorm/dataSource.ts
@@ -1,7 +1,12 @@
-import 'dotenv/config';
-
 import { DataSource } from 'typeorm';
 import ormConfig from './orm.config';
+import { config } from 'dotenv';
+
+const envFile =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+config({ path: envFile });
+
+console.log(process.env.DATABASE_URL);
 
 const dataSource = new DataSource({
   ...ormConfig,
